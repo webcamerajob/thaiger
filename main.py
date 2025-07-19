e545a05c0442389634bfd9cf0b668ffaeb5df162
webcamerajob <37417566+webcamerajob@users.noreply.github.com>
2025-07-19 20:56:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -322,7 +322,7 @@ def main():
     parser.add_argument("--base-url", type=str,
                         default="https://www.thethaiger.com",
                         help="WP site base URL")
-    parser.add_argument("--slug", type=str, default="news",
+    parser.add_argument("--slug", type=str, default="news/nationa",
                         help="Category slug")
     parser.add_argument("-n", "--limit", type=int, default=None,
                         help="Max posts to parse")
